d13588e774388584e8131847f0d025e5ea1a7b93
Devon Weller <dweller@devonweller.com>
2015-02-14 08:16:49 -0600

    die on deploy error

--- .deploy.sh
+++ .deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -e
+
 echo; echo "updating dependencies";
-/usr/local/bin/composer.phar install --prefer-dist
+/usr/local/bin/composer.phar install --prefer-dist 
 # ./artisan migrate
 
 echo; echo "updating bower dependencies";
