0e3a3f652d7917440ef18fd01a871e1d885319bd
Devon Weller <dweller@devonweller.com>
2015-05-30 11:06:04 -0500

    option to sweep payment and public addresses separately

--- app/Console/Commands/Bot/SweepBotCommand.php
+++ app/Console/Commands/Bot/SweepBotCommand.php
@@ -50,7 +50,9 @@ class SweepBotCommand extends Command {
     {
 
         return [
-            ['dry-run' , 'd',  InputOption::VALUE_NONE, 'Dry Run'],
+            ['dry-run' , 'd', InputOption::VALUE_NONE, 'Dry Run'],
+            ['payment' , 'p', InputOption::VALUE_NONE, 'Payment Address Only'],
+            ['public' ,  'u', InputOption::VALUE_NONE, 'Public Address Only'],
         ];
     }
 
@@ -71,6 +73,14 @@ class SweepBotCommand extends Command {
         $is_dry_run = !!$this->input->getOption('dry-run');
         if ($is_dry_run) { $this->comment("[Dry Run]"); }
 
+        // payment or public
+        $do_payment = !!$this->input->getOption('payment');
+        $do_public = !!$this->input->getOption('public');
+        if (!$do_payment AND !$do_public) {
+            $do_payment = true;
+            $do_public = true;
+        }
+
         // load the bot
         $bot_repository = app('Swapbot\Repositories\BotRepository');
         $bot = $bot_repository->findByID($bot_id);
@@ -85,13 +95,17 @@ class SweepBotCommand extends Command {
         $this->comment("Loading Bot Balances");
 
         try {
-            $payment_address_uuid = $bot['payment_address_id'];
-            $payment_balances = $xchain_client->getBalances($bot['payment_address']);
-            $this->info("payment address balances: ".json_encode($payment_balances, 192));
+            if ($do_payment) {
+                $payment_address_uuid = $bot['payment_address_id'];
+                $payment_balances = $xchain_client->getBalances($bot['payment_address']);
+                $this->info("payment address balances: ".json_encode($payment_balances, 192));
+            }
 
-            $public_address_uuid = $bot['public_address_id'];
-            $public_balances = $xchain_client->getBalances($bot['address']);
-            $this->info("public address balances: ".json_encode($public_balances, 192));
+            if ($do_public) {
+                $public_address_uuid = $bot['public_address_id'];
+                $public_balances = $xchain_client->getBalances($bot['address']);
+                $this->info("public address balances: ".json_encode($public_balances, 192));
+            }
         } catch (Exception $e) {
             $this->error($e->getMessage());
             if (!$is_dry_run) { return; }
@@ -99,16 +113,24 @@ class SweepBotCommand extends Command {
 
         // do sweeps
         if ($is_dry_run) {
-            $this->comment("[Dry Run] Would sweep all assets for payment address {$bot['payment_address']}");
-            $this->comment("[Dry Run] Would sweep all assets for public address {$bot['address']}");
+            if ($do_payment) {
+                $this->comment("[Dry Run] Would sweep all assets for payment address {$bot['payment_address']}");
+            }
+            if ($do_public) {
+                $this->comment("[Dry Run] Would sweep all assets for public address {$bot['address']}");
+            }
         } else {
-            $this->comment("Sweeping all assets for payment address {$bot['payment_address']}");
-            $result = $xchain_client->sweepAllAssets($payment_address_uuid, $destination);
-            $this->info("sweep payment address result: ".json_encode($result, 192));
+            if ($do_payment) {
+                $this->comment("Sweeping all assets for payment address {$bot['payment_address']}");
+                $result = $xchain_client->sweepAllAssets($payment_address_uuid, $destination);
+                $this->info("sweep payment address result: ".json_encode($result, 192));
+            }
 
-            $this->comment("Sweeping all assets for public address {$bot['address']}");
-            $result = $xchain_client->sweepAllAssets($public_address_uuid, $destination);
-            $this->info("sweep public address result: ".json_encode($result, 192));
+            if ($do_public) {
+                $this->comment("Sweeping all assets for public address {$bot['address']}");
+                $result = $xchain_client->sweepAllAssets($public_address_uuid, $destination);
+                $this->info("sweep public address result: ".json_encode($result, 192));
+            }
         }
 
         // $update_vars = ['state' => BotState::BRAND_NEW, 'balances' => $new_balances];
