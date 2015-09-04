18a088325d2dc875f0d5c4ab823ed77d1445d276
Devon Weller <devon@tokenly.co>
2015-09-04 16:03:12 -0500

    Register keen events handler

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -26,6 +26,7 @@ class EventServiceProvider extends ServiceProvider {
 		'Swapbot\Handlers\Events\BotIndexHandler',
 		'Swapbot\Handlers\Events\CustomerEmailHandler',
 		'Swapbot\Handlers\Events\AdminEmailHandler',
+		'Swapbot\Handlers\Events\KeenEventsHandler',
 	];
 
 
