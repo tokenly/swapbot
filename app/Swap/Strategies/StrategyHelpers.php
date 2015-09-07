63649b464840cf24f3b7e78761e741c6bacdc023
Devon Weller <devon@tokenly.co>
2015-09-07 08:01:40 -0500

    Support free asset names like A95428956661682201.

--- app/Swap/Strategies/StrategyHelpers.php
+++ app/Swap/Strategies/StrategyHelpers.php
@@ -34,10 +34,24 @@ class StrategyHelpers {
     public static function isValidAssetName($name) {
         if ($name === 'BTC') { return true; }
         if ($name === 'XCP') { return true; }
+
+        // check free asset names
+        if (substr($name, 0, 1) == 'A') { return self::isValidFreeAssetName($name); }
+
         if (!preg_match('!^[A-Z]+$!', $name)) { return false; }
         if (strlen($name) < 4) { return false; }
-        if (substr($name, 0, 1) == 'A') { return false; }
-        if (strlen($name) > 12) { return false; }
+
+        return true;
+    }
+
+    // allow integers between 26^12 + 1 and 256^8 (inclusive), prefixed with 'A'
+    public static function isValidFreeAssetName($name) {
+        if (substr($name, 0, 1) != 'A') { return false; }
+
+        $number_string = substr($name, 1);
+        if (!preg_match('!^\\d+$!', $number_string)) { return false; }
+        if (bccomp($number_string, "95428956661682201") < 0) { return false; }
+        if (bccomp($number_string, "18446744073709600000") > 0) { return false; }
 
         return true;
     }
---
+++ tests/tests/Validator/StrategyHelpersTest.php
@@ -0,0 +1,37 @@
+<?php
+
+use Swapbot\Swap\Strategies\StrategyHelpers;
+use \PHPUnit_Framework_Assert as PHPUnit;
+
+class StrategyHelpersTest extends TestCase {
+
+    protected $use_database = false;
+
+    public function testValidAssetNames() {
+        $this->assertValidAsset('BTC');
+        $this->assertValidAsset('XCP');
+        $this->assertValidAsset('SOUP');
+        $this->assertInvalidAsset('ABLE');
+
+        $this->assertInvalidAsset('A1234567890');
+        $this->assertInvalidAsset('A1234567890X');
+        $this->assertInvalidAsset('A95428956661682199');
+        $this->assertInvalidAsset('A95428956661682200');
+        $this->assertValidAsset(  'A95428956661682201');
+        $this->assertValidAsset('A18446744073709600000');
+        $this->assertInvalidAsset('A18446744073709600001');
+    }
+
+    ////////////////////////////////////////////////////////////////////////
+    
+    
+    protected function assertValidAsset($name) {
+        PHPUnit::assertTrue(StrategyHelpers::isValidAssetName($name), "The name $name was not valid and it should have been");
+    }
+
+    protected function assertInvalidAsset($name) {
+        PHPUnit::assertFalse(StrategyHelpers::isValidAssetName($name), "The name $name was valid and it should not have been");
+    }
+
+
+}
