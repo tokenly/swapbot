4cc3b86d9e6a8d0a9e3fbe7573fa3393176e1c4d
Devon Weller <devon@tokenly.co>
2015-09-18 07:05:53 -0500

    additional slugifier work

--- app/Util/Slug/Slugifier.php
+++ app/Util/Slug/Slugifier.php
@@ -17,5 +17,13 @@ class Slugifier {
         return (preg_match('!^[a-z0-9-]+$!', $slug) AND $len >= self::MIN_LENGTH AND $len <= self::MAX_LENGTH);
     }
 
+    public static function slugify($str) {
+        $slug = strtolower(trim($str));
+        $slug = preg_replace("/[^a-z0-9[:space:]\/s-]/", "", $slug);
+        $slug = preg_replace("/(-| |\/)+/","-",$slug);
+
+        return $slug;
+    }
+
 }
 
