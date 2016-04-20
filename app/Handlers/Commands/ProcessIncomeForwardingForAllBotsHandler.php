5e513fb9d3ba75e1929cce235444aa78d3aafa08
Devon Weller <devon@tokenly.co>
2016-04-20 06:49:03 -0500

    When forwarding income, account for the transaction fee before sending the amount.
    Closes #230

--- app/Handlers/Commands/ProcessIncomeForwardingForAllBotsHandler.php
+++ app/Handlers/Commands/ProcessIncomeForwardingForAllBotsHandler.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Log;
 use Swapbot\Commands\ProcessIncomeForwardingForAllBots;
+use Swapbot\Handlers\Commands\ForwardPaymentHandler;
 use Swapbot\Repositories\BotRepository;
 use Swapbot\Swap\DateProvider\Facade\DateProvider;
 use Swapbot\Swap\Logger\BotEventLogger;
@@ -54,30 +55,35 @@ class ProcessIncomeForwardingForAllBotsHandler {
                             // send the transaction
                             $asset = $income_rule_config['asset'];
                             $destination = $income_rule_config['address'];
-                            $quantity = $this->buildQuantityToForward($bot_balance, $income_rule_config);
                             $fee = $bot['return_fee'];
-
-
-                            // don't do the same income forwarding send within 60 minutes
-                            $cache_key = $bot['uuid'].','.$income_rule_config['asset'];
-                            $send_uuid = Cache::get($cache_key);
-                            if (!$send_uuid) {
-                                $send_uuid = DateProvider::microtimeNow();
-                                Cache::put($cache_key, $send_uuid, 60);
+                            $quantity = $this->buildQuantityToForward($bot_balance, $income_rule_config, $fee);
+                            if ($quantity < ForwardPaymentHandler::DEFAULT_REGULAR_DUST_SIZE) {
+                                $err_msg = "Unable to forward income because the amount was too small.";
+                                EventLog::logError('income.forward.insufficient', $err_msg, ['id' => $bot['id'], 'quantity' => $quantity, 'asset' => $asset, ]);
                             }
 
-                            $request_id = RequestIDGenerator::generateSendHash('incomeforward'.','.$bot['uuid'].','.$send_uuid, $destination, $quantity, $asset);
-                            EventLog::log('bot.income.process', array_merge(['name' => $bot['name'], 'id' => $bot['id']], compact('destination', 'quantity', 'asset')));
-                            $send_result = $this->xchain_client->sendConfirmed($bot['public_address_id'], $destination, $quantity, $asset, $fee, null, $request_id);
+                            if ($quantity >= ForwardPaymentHandler::DEFAULT_REGULAR_DUST_SIZE) {
+                                // don't do the same income forwarding send within 60 minutes
+                                $cache_key = $bot['uuid'].','.$income_rule_config['asset'];
+                                $send_uuid = Cache::get($cache_key);
+                                if (!$send_uuid) {
+                                    $send_uuid = DateProvider::microtimeNow();
+                                    Cache::put($cache_key, $send_uuid, 60);
+                                }
 
-                            // log the event
-                            $this->bot_event_logger->logIncomeForwardingResult($bot, $send_result, $destination, $quantity, $asset);
+                                $request_id = RequestIDGenerator::generateSendHash('incomeforward'.','.$bot['uuid'].','.$send_uuid, $destination, $quantity, $asset);
+                                EventLog::log('bot.income.process', array_merge(['name' => $bot['name'], 'id' => $bot['id']], compact('destination', 'quantity', 'asset')));
+                                $send_result = $this->xchain_client->sendConfirmed($bot['public_address_id'], $destination, $quantity, $asset, $fee, null, $request_id);
 
-                            // clear the send cache
-                            Cache::forget($cache_key);
+                                // log the event
+                                $this->bot_event_logger->logIncomeForwardingResult($bot, $send_result, $destination, $quantity, $asset);
 
-                            // update the balance later
-                            $was_forwarded = true;
+                                // clear the send cache
+                                Cache::forget($cache_key);
+
+                                // update the balance later
+                                $was_forwarded = true;
+                            }
                         } catch (Exception $e) {
                             // log failure
                             $this->bot_event_logger->logIncomeForwardingFailed($bot, $e);
@@ -100,21 +106,34 @@ class ProcessIncomeForwardingForAllBotsHandler {
 
     }
 
-    protected function buildQuantityToForward($bot_balance, $income_rule_config) {
+    protected function buildQuantityToForward($bot_balance, $income_rule_config, $fee) {
         // send as much as we can to get below the threshold
-        $threshold = $income_rule_config['minThreshold'];
+        $threshold  = $income_rule_config['minThreshold'];
         $chunk_size = $income_rule_config['paymentAmount'];
+        $asset      = $income_rule_config['asset'];
 
         // if the bot balance is exactly equal to the threshold
         //   then forward $chunk_size
-        if ($bot_balance == $threshold) { return $chunk_size; }
+        if ($bot_balance == $threshold) {
+            $quantity = $chunk_size;
+        }
 
-        // do as many chunks as possible at once
-        $number_of_chunks = ceil(($bot_balance - $threshold) / $chunk_size);
-        $quantity = $number_of_chunks * $chunk_size;
+        if ($bot_balance != $threshold) {
+            // do as many chunks as possible at once
+            $number_of_chunks = ceil(($bot_balance - $threshold) / $chunk_size);
+            $quantity = $number_of_chunks * $chunk_size;
+        }
 
         // never send more than we have, even if it was configured that way
         if ($quantity > $bot_balance) { $quantity = $bot_balance; }
+
+        // When sending BTC, always account for the fee
+        if ($asset == 'BTC') {
+            if ($quantity + $fee > $bot_balance) {
+                $quantity = $bot_balance - $fee;
+                if ($quantity < 0) { $quantity = 0; }
+            }
+        }
 
         return $quantity;
     }
